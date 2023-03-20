f113c30e7e0abbcd178c9fc8d7724bd7a7b71ff0
AlexandreHilario <alexandreds969@gmail.com>
2023-03-20 17:49:46 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className="tittle">Alexandre Hilário</h1>
+      <p className="subTittle">Desenvolvedor indie (Game designer / Reader Vagabound)</p>
+
+      <nav className="navegationTab">
+        <a href="https://github.com/AlexandreHilario" target={"_blank"}>Github | </a>
+        <a href="https://docs.google.com/document/d/1wsv4Fo0dIx1DZfiyyZqzX4L0nPNVE1tgZXFAn3q62n4/edit?usp=sharing" target={"_blank"}>Resume</a>
+      </nav>
+
+      <main className="main">
+        <p>Tenho 19 anos e estou começando na área de game design. Meu foco são games em 2D, principalmente em pixel art.<br>
+        </br> <span>Alone</span> é meu primeiro projeto feito de forma independente, utilizando a engine <a href="https://godotengine.org/" target={"_blank"}>Godot</a>
+        com o suporte a C#.</p>
+      </main>
+      
     </div>
+   
   );
 }
 
