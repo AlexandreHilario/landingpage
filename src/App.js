59a402c85eb482a0d7036ee803c4812dda286bdf
AlexandreHilario <alexandreds969@gmail.com>
2023-05-21 15:51:23 -0300

    player de videos e mudanças no css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import minhaImagem from './Assests/capa.jpg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={minhaImagem} className="App-logo" alt="capa" />
-        <p>
-          Hollow Knight o melhor game já feito
-        </p>
-        <a
-          className="App-link"
-          href="https://store.steampowered.com/app/367520/Hollow_Knight/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jogue Agora
-        </a>
-      </header>
+      <img src={minhaImagem} className="App-logo" alt="capa" />
+      <a
+        className="App-link"
+        href="https://store.steampowered.com/app/367520/Hollow_Knight/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Jogue Agora
+      </a>
+
+      <ReactPlayer url='https://www.youtube.com/embed/UAO2urG23S4' />
+
     </div>
   );
 }
