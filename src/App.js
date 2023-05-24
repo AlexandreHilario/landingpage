e2c8594e8fd3cd49aa5771832949e855d2fcb03d
AlexandreHilario <alexandreds969@gmail.com>
2023-05-24 15:37:13 -0300

    implementação do header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import minhaImagem from './Assests/capa.jpg';
+import logo from './Assests/SS_Cover_Art.webp';
+import imageBackground from './Assests/maxresdefault.jpg'
+import titulo from './Assests/pngegg.png'
 import './App.css';
-import ReactPlayer from 'react-player';
 
 function App() {
   return (
     <div className="App">
-      <img src={minhaImagem} className="App-logo" alt="capa" />
-      <a
-        className="App-link"
-        href="https://store.steampowered.com/app/367520/Hollow_Knight/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Jogue Agora
-      </a>
 
-      <ReactPlayer url='https://www.youtube.com/embed/UAO2urG23S4' />
+      <header class='Card-header'>
+        <img class='Icon' src={logo} alt='Logo'/>
+        <h1 class='title'>TEAM CHERRY HUB</h1>
+        <ul class='list'>
+          <li>News</li>
+          <li>Reviews</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+      <div class='main'>
+        <img class='backgroud-main' src={imageBackground} alt='imagem background'/>
+        <img class='main-title' src={titulo} alt='imagem titulo'/>
+        <p class='apresentation'>
+          A Team Cherry é um pequeno estúdio de desenvolvimento de jogos independentes sediado em Adelaide, na Austrália.
+          Composto pelos talentosos desenvolvedores William Pellen, Ari Gibson e David Kazi, o 
+          estúdio ganhou reconhecimento mundial com o lançamento do aclamado jogo Hollow Knight em 2017. 
+          Esse hub foi feito na intenção de trazer notícias e algumas reviews sobre a Team Cherry.</p>
+
+      </div>
 
     </div>
   );
