ea61d7f0c7cfc97d19a88625c176bab228fb61c1
AlexandreHilario <alexandreds969@gmail.com>
2023-05-31 15:17:42 -0300

    section 3 iniciada

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <li>Contact</li>
         </ul>
       </header>
+
       <section class='section1'>
         <img class='background-main' src={imageBackground} alt='imagem background' />
         <div class="title-container">
@@ -26,8 +27,8 @@ function App() {
           A Team Cherry é um pequeno estúdio de desenvolvimento de jogos independentes sediado em Adelaide, na Austrália.
           Composto pelos talentosos desenvolvedores William Pellen, Ari Gibson e David Kazi, o
           estúdio ganhou reconhecimento mundial com o lançamento do aclamado jogo Hollow Knight em 2017.
-          Esse hub foi feito na intenção de trazer notícias e algumas reviews sobre a Team Cherry.</p>
-
+          Esse hub foi feito na intenção de trazer notícias e algumas reviews sobre a Team Cherry.
+        </p>
       </section>
 
       <section class='section2'>
@@ -41,12 +42,54 @@ function App() {
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen>
         </iframe>
-        <p class="comments1">Até a data de corte em setembro de 2021, Hollow Knight: Silksong ainda não havia sido lançado. Desde o anúncio oficial
+
+        <p class="comments1">
+           Até a data de corte em setembro de 2021, Hollow Knight: Silksong ainda não havia sido lançado. Desde o anúncio oficial
            do jogo em fevereiro de 2019, a Team Cherry tem trabalhado no desenvolvimento do Silksong. No final de 2022, foi vazado 
            que no primeiro semestre de 2023, o game seria lançado, mas em maio, um dos desenvolvedores publicou no seu twitter que
            o game seria adiado novamente e apenas teriamos alguma notícia perto do seu lançamento oficial.<br></br><br></br>Única coisa que 
            temos é a gameplay e seu trailer. A nova protagonista que aparece no game anterior, a Honert, ira nos acompanhar na nova 
-           aventura em Hallownest, com novos inimigos, novos segredos e uma nova narrativa.</p>
+           aventura em Hallownest, com novos inimigos, novos segredos e uma nova narrativa.
+        </p>
+
+        <div class="buttom1">
+          <a href='https://store.steampowered.com/app/1030300/Hollow_Knight_Silksong/'
+             target='_blank'
+             rel="noreferrer"
+             class="link1">
+            LINK PARA ADICIONAR NA STEAM
+          </a>
+        </div>
+      </section>
+
+      <section class="section3">
+        <iframe
+          class="trailer-two"
+          title="trailer silksong"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/UAO2urG23S4"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+        </iframe>
+
+        <p class="comments2">Hollow Knight é um jogo de plataforma e aventura que transporta os jogadores para um mundo sombrio 
+        e misterioso, onde assumem o papel de um pequeno cavaleiro em busca de respostas e aventuras em um reino em ruínas chamado 
+        Hallownest. Com uma narrativa envolvente, jogabilidade desafiadora, personagens fascinantes, exploração meticulosa, belo 
+        design de arte e uma atmosfera única, o jogo oferece uma experiência memorável que cativa os jogadores desde o seu 
+        lançamento em 2017.<br></br><br></br>Ao jogarmos, assumimos o controle sobre um pequeno guerreiro sem nome, que visita a cidade 
+        de Hallownets e descobrimos os seus terríveis segredos, enquanto exploramos e adquirimos habilidades novas.
+        </p>
+
+        <div class="buttom2">
+          <a href='https://store.steampowered.com/app/367520/Hollow_Knight/'
+             target='_blank'
+             rel="noreferrer"
+             class="link2">
+            LINK PARA ADICIONAR NA STEAM
+          </a>
+        </div>
       </section>
 
     </div>
